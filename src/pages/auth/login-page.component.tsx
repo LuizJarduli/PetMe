81006d507383ee6422dc5a0ef3956000633bc359
LuizJarduli <luizjarduli@gmail.com>
2022-03-15 23:23:34 -0300

    feat: login page component, in progress

---
+++ src/pages/auth/login-page.component.tsx
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+
+/**
+ * Componente da tela de login
+ * @author Luiz Miguel
+ * @since 03/2022
+ */
+export class LoginPageComponent extends Component {
+
+    /**
+     * Renderiza os elementos da página
+     */
+    render(): JSX.Element {
+        return(
+            <h1>Tela de Login será criada aqui</h1>
+        )
+    }
+}
