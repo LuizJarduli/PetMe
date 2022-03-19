2afa36ffcaac881fcbf205705b67d57f3bdd40b9
LuizJarduli <luizjarduli@gmail.com>
2022-03-19 17:07:56 -0300

    feat: create 404 page, user, and form component, in progress

---
+++ src/pages/cadastros/cadastro-usuario/cadastro-usuario.component.tsx
@@ -0,0 +1,9 @@
+import { Component, ReactNode } from 'react';
+
+export class CadastroUsuarioComponent extends Component {
+    render(): ReactNode {
+        return(
+            <h1>Aqui vai a tela de cadastro</h1>
+        );
+    }
+}
---
+++ src/pages/no-page/no-page.component.tsx
@@ -0,0 +1,9 @@
+import { Component, ReactNode } from 'react';
+
+export class NoPageComponent extends Component {
+    render(): ReactNode {
+        return(
+            <h1>404, :(</h1>
+        );
+    }
+}
