fab749ebbbe28282bd5e7a2bd043d23869f4dd4a
LuizJarduli <luizjarduli@gmail.com>
2022-03-19 14:35:04 -0300

    style: global styles, in progress

---
+++ src/styles/global.ts
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from 'styled-components';
+
+/**
+ * Estilos Globais do App
+ */
+export default createGlobalStyle`
+    /**
+    * Cores do App
+    */
+    :root {
+        --primary: ##FAFAFA;
+        --secondary: #FFFFFE;
+        --black: #000000;
+        --green: #FFFFFE;
+        --grey: #F2F2F3;
+        --white: #FFFFFF;
+        --light-grey: #C2C2C2;
+        --card-size-sm: 300px;
+        --card-size-md: 430px;
+        --card-size-xl: 800px;
+        --padding-xs: 8px;
+        --padding-sm: 16px;
+        --padding-md: 24px;
+        --padding-xl: 32px;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html, body, #root {
+        max-height: 100vh;
+        max-width: 100vw;
+        height: 100%;
+        width: 100%;
+    }
+
+    *, button, input {
+        border: 0;
+        background: none;
+        font-family: 'Noto Sans', Roboto, 'Helvetica Neue', Arial, sans-serif;
+    }
+
+    body {
+        background-color: var(--primary);
+    }
+`
