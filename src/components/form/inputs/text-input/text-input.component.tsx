17d122fe92cdd6a7ca15ab78e50af21dcbdcb0b3
LuizJarduli <luizjarduli@gmail.com>
2022-03-24 00:27:43 -0300

    feat: text-input component

--- src/components/form/inputs/text-input/text-input.component.tsx
+++ src/components/form/inputs/text-input/text-input.component.tsx
@@ -1,10 +1,13 @@
-import { Component } from 'react';
+import { IInputProperties } from '../input-properties.interface';
+import { Input } from '../input.component';
 
 /**
  * Componente de Input de Texto Genérico do App
  * @author Luiz Miguel
  * @since 03/2022
  */
-export class TextInputComponent extends Component {
-    
-}
+export class TextInputComponent extends Input {
+    constructor(props: IInputProperties) {
+        super(props);
+    }
+ }
