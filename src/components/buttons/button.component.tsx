a77e8a6464dbac584b364441846c64c7ce49e004
LuizJarduli <luizjarduli@gmail.com>
2022-04-17 15:25:46 -0300

    feat: navigation to user inclusion form-page

--- src/components/buttons/button.component.tsx
+++ src/components/buttons/button.component.tsx
@@ -1,18 +1,19 @@
-import { Component, ReactNode } from 'react';
+import { Component } from 'react';
 import { IButtonProperties } from './button-properties.interface';
 import { StyledButton } from './style';
 
-export class ButtonComponent extends Component<IButtonProperties, HTMLButtonElement> {
-    constructor(props: IButtonProperties & HTMLButtonElement) {
+export class ButtonComponent extends Component<IButtonProperties> {
+    constructor(props: IButtonProperties) {
         super(props);
     }
 
     /**
      * Renderiza os Elementos da página
      */
-    render(): ReactNode {
+    render(): JSX.Element {
         return (
             <StyledButton
+                {...this.props}
                 label={this.props.label}
                 name={this.props.name}
                 color={this.props.color}>
