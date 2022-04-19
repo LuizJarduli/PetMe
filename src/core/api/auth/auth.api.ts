6d18ae796141ed28caf131252888a7f2b9b712e8
LuizJarduli <luizjarduli@gmail.com>
2022-04-18 21:48:13 -0300

    feat: AuthApi class

---
+++ src/core/api/auth/auth.api.ts
@@ -0,0 +1,22 @@
+import { ApiService } from './../api';
+import { ILoginResponse, ILoginProperties } from './auth.api.properties';
+
+/**
+ * Classe que Realiza requisições referentes a autenticação/Login na API do sistema
+ * @author Luiz Miguel
+ * @since 04/2022
+ */
+export class AuthApi {
+
+    /**
+     * Efetua o Login na API do sistema
+     * @param params 
+     */
+    public static login(params: ILoginProperties): Promise<ILoginResponse> {
+        return new Promise<ILoginResponse>((resolve, reject) => {
+            ApiService.getInstance().setMethod('POST').call('https://restful-booker.herokuapp.com/auth', params)
+                .then((response: ILoginResponse) => resolve(response))
+                .catch((error: any) => reject(error));
+        });
+    }
+}
--- src/pages/auth/login-page.component.tsx
+++ src/pages/auth/login-page.component.tsx
@@ -5,6 +5,8 @@ import { CardComponent } from '../../components/containers/card/card.component';
 import { FormComponent } from '../../components/form/form.component';
 import { PasswordInputComponent } from '../../components/form/inputs/password-input/password-input.component';
 import { TextInputComponent } from '../../components/form/inputs/text-input/text-input.component';
+import { AuthApi } from '../../core/api/auth/auth.api';
+import { ILoginProperties, ILoginResponse } from '../../core/api/auth/auth.api.properties';
 import { Column, Container, ForgetPasswordLabel, ForgetPasswordOption } from './style';
 
 /**
@@ -19,8 +21,16 @@ export class LoginPageComponent extends Component {
 
     state: { user: null, error: null, redirect: null};
 
+    /**
+     * Realiza o Login do usuário ao dar submit no form
+     *
+     * @param formData dados de Login
+     */
     private handleLoginFormSubmit(formData: any): void {
-        console.log(formData); // TODO: criar rotina de login
+        console.log('dados de submit', formData); // TODO: criar rotina de login
+        AuthApi.login({ username: formData.userName.value, password: formData.userPassword.value})
+            .then((response: ILoginResponse) => console.log('sucesso', response))
+            .catch((error) => console.log('erro', error));
     }
 
     /**
@@ -37,7 +47,7 @@ export class LoginPageComponent extends Component {
     componentDidMount(): void {
         document.addEventListener('onFormSubmit', (event) => {
             event.stopPropagation();
-            this.handleLoginFormSubmit(event)
+            this.handleLoginFormSubmit((event as CustomEvent).detail)
         });
     }
 
@@ -45,7 +55,7 @@ export class LoginPageComponent extends Component {
      * Chamado imediatamente após o componente ser destruído
      */
     componentWillUnmount(): void {
-        document.removeEventListener('onFormSubmit', (event) => this.handleLoginFormSubmit(event));
+        document.removeEventListener('onFormSubmit', (event) => this.handleLoginFormSubmit((event as CustomEvent).detail));
     }
 
     /**
