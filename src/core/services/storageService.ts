7b5e04a28966dffb2825a2449dfae359c8d34c9f
Oaní da Silva da Costa <oani_costa@outlook.com.br>
2022-05-24 20:08:01 -0300

    feat: storageService

---
+++ src/core/services/storageService.ts
@@ -0,0 +1,33 @@
+import { IUserPropertiesModel } from '../api/cadastro/cadastro.api.properties';
+
+/**
+ * Service de armazenamento do app, guarda infos no sessionStorage
+ * @author Luiz Miguel
+ * @since 05/2022
+ */
+export class StorageService {
+    constructor() { }
+
+    /**
+     * Retorna a instância da classe
+     */
+    public static getInstance(): StorageService {
+        return new StorageService();
+    }
+
+    /**
+     * Armazena os dados do usuário logado
+     *
+     * @param userData usuário logado
+     */
+    public setUser(userData: any) {
+        sessionStorage.setItem('userLogged', JSON.stringify(userData));
+    }
+
+    /**
+     * Recupera os dados do usuário logado
+     */
+    public getUser(): IUserPropertiesModel {
+        return JSON.parse(sessionStorage.getItem('userLogged') as string);
+    }
+}
